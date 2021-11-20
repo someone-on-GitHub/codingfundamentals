078cb942b57889807e9d9fad7aa79800c055c046
= <=>
2021-11-20 09:41:56 -0600

    jacks changes

--- Control Structures/IfThenElse.py
+++ Control Structures/IfThenElse.py
@@ -1,15 +1,23 @@
-animal = "dog"
+foundanimal = False
+while foundanimal == False:
+    print("Enter the animal")
+    animal = input()
 
-if animal == "dog":
-    print("the animal is a dog")
-
-
-animal = "Jack"
-if animal == "cat":
-    print("the animal is a cat")
-elif animal == "bird":
-    print("the animal is a bird")
-elif animal == "hippo":
-    print("the animal is a hippo")
-else:
-    print("I have no idea what this animal is!")
+    if animal == "dog":
+        foundanimal = True
+        print("the animal is a dog")
+    elif animal == "cat":
+        foundanimal = True
+        print("the animal is a cat")
+    elif animal == "bird":
+        foundanimal = True
+        print("the animal is a bird")
+    elif animal == "hippo":
+        foundanimal = True
+        print("the animal is a hippo")
+    else:
+        print("I have no idea what this animal is!")
+        print("do you want to try another animal, Y or N?")
+        tryagain = input()
+        if tryagain.upper() != "Y":
+            foundanimal = True
