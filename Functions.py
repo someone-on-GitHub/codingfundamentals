af8496766415ba1d4d33ae7237532192b75a1bb9
= <=>
2021-11-21 08:24:22 -0600

    added iterators and functions

---
+++ Functions.py
@@ -0,0 +1,8 @@
+# a function is a set of code that does a specific thing
+
+def add_two_numbers(number1, number2):
+    return number1 + number2
+
+# execute the function
+the_sum_of_one_and_two = add_two_numbers(1, 2)
+print(the_sum_of_one_and_two)
